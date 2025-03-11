27414576944f410895de3944fc78820fe0cf7537
EK <service@un-no.com>
2025-03-11 12:04:01 +0700

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ui' as ui;
+import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // ignore: prefer_const_constructors
-
 
 void main() {
   runApp(const MyApp());
@@ -15,21 +16,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Google Maps Test'),
@@ -47,52 +38,172 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<List<double>> _coords = [[ -71.975556, -47.0 ],[ -72.025, -47.183333 ], [ -72.333333, -47.416667 ], [ -72.55, -47.925 ], [ -72.366667, -48.116667 ], [ -72.366667, -48.316667 ], [ -72.608333, -48.45 ], [ -72.6, -48.783333 ], [ -73.466667, -49.3 ], [ -73.566667, -49.55 ], [ -73.541667, -50.15 ], [ -73.166667, -50.8 ], [ -72.383333, -50.683333 ], [ -72.45, -51.566667 ], [ -71.916667, -52.0 ], [ -70.0, -52.0 ], [ -68.55, -52.291667 ], [ -68.6, -52.666667 ], [ -68.6, -54.9 ], [ -67.966667, -54.883333 ], [ -67.116667, -54.933333 ], [ -66.833333, -55.0 ], [ -66.666667, -55.133333 ], [ -66.078333, -55.183333 ], [ -65.726667, -55.381667 ], [ -65.726667, -56.38 ], [ -67.266667, -56.38 ], [ -67.266667, -58.351667 ], [ -53.0, -58.351667 ], [ -53.0, -90.0 ], [ -90.0, -90.0 ], [ -90.0, -47.0 ], [ -71.975556, -47.0 ] ];
+  Map<String, Marker> markers = <String, Marker>{};
+  Map<String, Polyline> polylines = <String, Polyline>{};
+
+  _onMapCreate(GoogleMapController controller) async {
+    var location = LatLng(14.0, 101.0);
+
+    var markers = this.markers;
+    markers['test2'] = Marker(
+      markerId: MarkerId('test'),
+      zIndex: 8,
+      icon: await iconMarker(),
+      position: location,
+    );
+    //markers['test'] =
+    //    Marker(markerId: MarkerId('test'), zIndex: 8, position: location);
+
+    var polylines = this.polylines;
+
+    final List<LatLng> points =
+        "18.6,97.2 20.4,99.9 19.5,101.3 17.6,101.1 18.5,103.2 16.3,105.0 "
+                "15.7,105.6 14.3,105.3 14.2,103.1 13.3,102.3 11.8,102.7 13.6,100.7 "
+                "13.9,100.1 12.3,99.9"
+            .split(' ')
+            .map((pair) {
+      var parts = pair.split(',');
+      return LatLng(double.parse(parts[0]), double.parse(parts[1]));
+    }).toList();
+    polylines['test'] = Polyline(
+      polylineId: PolylineId('testPolyline'),
+      color: Colors.red,
+      width: 2,
+      points: points,
+    );
+
+    setState(() {
+      this.markers = markers;
+      this.polylines = polylines;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // create polylines;
-    Set<Polyline>? _polylines = {};
-
-    _polylines.add(
-      Polyline(
-        polylineId: PolylineId('testPolyline'),
-        color: Colors.red,
-        width: 2,
-        points: [
-          for (var i = 0; i < _coords.length; i++)
-            LatLng(_coords[i][1], _coords[i][0]),
-        ]
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          
-          Expanded(
-            child: GoogleMap(
-              polylines: _polylines,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(-71.975556, -47.0),
-                zoom: 0,
-              ),
-              trafficEnabled: false,
-              mapToolbarEnabled: false,
-              zoomControlsEnabled: false,
-              myLocationButtonEnabled: false,
-              buildingsEnabled: false,
-              gestureRecognizers: Set()
-              ..add(Factory<EagerGestureRecognizer>(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Expanded(
+              child: GoogleMap(
+                polylines: Set<Polyline>.of(polylines.values),
+                markers: Set<Marker>.of(markers.values),
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(14.7, 100.7),
+                  zoom: 5,
+                ),
+                onMapCreated: _onMapCreate,
+                trafficEnabled: false,
+                mapToolbarEnabled: false,
+                zoomControlsEnabled: false,
+                myLocationButtonEnabled: false,
+                buildingsEnabled: false,
+                gestureRecognizers: Set()
+                  ..add(Factory<EagerGestureRecognizer>(
                       () => EagerGestureRecognizer())),
+              ),
             ),
-          ),
-        ]
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ]), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Future<BitmapDescriptor> iconMarker() async {
+  bool isNoNumber = false;
+  Color _statusColor = Colors.red;
+
+  var wMarker = 300.0;
+  var wMarker_2 = wMarker / 2;
+  var wMarker_4 = wMarker / 4;
+  var wMarker_6 = wMarker / 6;
+  var wMarker_8 = wMarker / 8;
+  var wMarker_10 = wMarker / 10;
+  var wBorderwidth = wMarker / 15;
+  var wBorderwidth_2 = wBorderwidth / 2;
+  var hMarker = wMarker + wMarker_2 + (isNoNumber ? 0 : wMarker_4);
+
+  Paint sBorderFill = Paint()
+    ..color = _statusColor
+    ..style = PaintingStyle.fill;
+  Paint sBorderStroke = Paint()
+    ..color = _statusColor
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = wBorderwidth;
+
+  Paint sCircleFill = Paint()..color = Colors.white;
+
+  ui.PictureRecorder recorder = ui.PictureRecorder();
+  Canvas canvas = Canvas(recorder);
+
+  canvas.drawRect(
+      Rect.fromLTWH(0, 0, wMarker, hMarker), Paint()..color = Colors.orange);
+
+  // ----- CIRCLE
+
+  final Offset circleCenter =
+      Offset(wMarker_2, isNoNumber ? wMarker_2 * 1.8 : wMarker + wMarker_8);
+  final double circleRadius = wMarker_2 - wBorderwidth;
+
+  var mArrow = Path()
+    ..moveTo(
+        wMarker_2 - wMarker_4, wMarker + (isNoNumber ? wMarker_8 : wMarker_4))
+    ..lineTo(
+        wMarker_2 + wMarker_4, wMarker + (isNoNumber ? wMarker_8 : wMarker_4))
+    ..lineTo(wMarker_2, hMarker)
+    ..close();
+  canvas.drawPath(mArrow, sBorderFill);
+
+  canvas.drawCircle(circleCenter, wMarker_2, sBorderFill);
+  canvas.drawCircle(circleCenter, wMarker_2 - wBorderwidth, sCircleFill);
+
+  // ----- ICON / IMG
+
+  var number = "สวัสดี";
+  var numberHeight = wMarker / 3;
+
+  canvas.drawRect(
+      Offset(0, wMarker_2) & ui.Size(wMarker, numberHeight),
+      Paint()
+        ..color = Colors.yellow
+        ..style = PaintingStyle.fill);
+
+  canvas.drawRect(
+      Offset(0, wMarker_2) & ui.Size(wMarker, numberHeight),
+      Paint()
+        ..color = Colors.red
+        ..strokeWidth = 5
+        ..style = PaintingStyle.stroke);
+
+  TextSpan title = TextSpan(
+    style: TextStyle(
+        color: Colors.red,
+        backgroundColor: Colors.transparent,
+        fontSize: numberHeight * 0.55, // ขนาดของฟอนต์สัมพันธ์กับ numberHeight
+        fontWeight: FontWeight.bold),
+    text: number,
+  );
+
+  TextPainter tp = TextPainter(
+      text: title,
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr);
+  tp.layout();
+
+  final double textX = (wMarker - tp.width) / 2;
+  final double textY = wMarker_2 +
+      (numberHeight - tp.height) / 2; // ทำให้ข้อความอยู่ตรงกลางแนวตั้ง
+
+  tp.paint(canvas, Offset(textX, textY));
+
+  // ----- ARROW
+
+  ui.Picture p = recorder.endRecording();
+  ByteData? pngBytes = await (await p.toImage(wMarker.toInt(), hMarker.toInt()))
+      .toByteData(format: ui.ImageByteFormat.png);
+  Uint8List data = Uint8List.view(pngBytes!.buffer);
+
+  return BitmapDescriptor.fromBytes(data);
 }
